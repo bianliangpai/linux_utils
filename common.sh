01537a65889489afbafb750b2eb787b78789ec15
bianliangpai <bianliangpai@gmail.com>
2021-05-11 23:14:33 +0800

    [NEW]: add apt_clear(), apt_refresh(), cl()

--- common.sh
+++ common.sh
@@ -1,12 +1,42 @@
 #! /bin/bash
 
+apt_clear() {
+    sudo killall apt
+    sudo rm /var/lib/apt/lists/lock
+    sudo rm /var/cache/apt/archives/lock
+    sudo rm /var/lib/dpkg/lock*
+    sudo dpkg --configure -a
+}
+
+apt_refresh() {
+    apt_clear
+
+    sudo apt-get update
+    echo Y | sudo apt-get upgrade
+}
+
+cl() {
+    FILEPATHS=$(find | grep -P '^((?!/build/.)*$')
+    cnt=0
+    while read -r line; do
+        if [ ! -d $line ]; then
+            cur=$(cat $line | wc -l)
+            printf "%6d $line\n" $cur
+
+            cnt=$(expr $cnt + $cur)
+        fi
+    done <<< "$FILEPATHS"
+
+    printf "%6d total\n" $cnt
+}
+
 function get_cpucore_number() {
   printf $( grep ^processor /proc/cpuinfo | \
           wc -l |                           \
           awk '{print $1}' )
 }
 
-# 
+#
 # $ cat /proc/stat
 # cpu  15543 334  4963   142337 3413   0   180    0     0 0
 #      user  nice system idle   iowait irq softrq steal
@@ -15,7 +45,7 @@ function get_cpucore_number() {
 #           = 1 - (idle2+iowait2-idle1-iowait1) / (total_time2-total_time1)
 #
 # tips: totol_time=user+nice+system+idle+iowait+irq+sortrq
-# 
+#
 function get_current_cpu_loading() {
   # printf $( top -bn1 | grep "Cpu(s)" |        \
   #         sed "s/.*, *\([0-9.]*\) id.*/\1/" | \
